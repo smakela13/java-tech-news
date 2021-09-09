4cfbf86696cfc3e5a59f44d15dd92bea245bfb55
SMakela <73409799+smakela13@users.noreply.github.com>
2021-09-09 17:30:49 -0400

    Fixed imports and formatting - Added insomnia.json

--- src/main/java/com/technews/controller/UserController.java
+++ src/main/java/com/technews/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.technews.controller;
 
+import com.technews.model.Post;
+import com.technews.model.User;
+import com.technews.repository.UserRepository;
+import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 @RestController
 public class UserController {
